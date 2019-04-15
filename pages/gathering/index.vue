04642372ac3d8eec9ccf8dffe06cb9024f9069e2
amefan <1257096364@qq.com>
2019-04-15 14:49:46 +0800

    静态资源引入

---
+++ pages/gathering/index.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <div>活动列表</div>
+    <div v-for="(item,index) in list" :key="index">
+       <nuxt-link :to="'/gathering/item/'+item.id"> {{item.name}}</nuxt-link>
+        </div>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    asyncData () {
+    return axios.get(`http://localhost:9003/gathering`)
+    .then((res) => {
+      return { list: res.data.data }
+    })
+  }
+}
+</script>
---
+++ pages/gathering/item/_id.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3>{{pojo.name}}</h3>
+        <hr>
+        <div>{{pojo.detail}}</div>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    asyncData ({ params }) {
+    return axios.get(`http://localhost:9003/gathering/${params.id}`)
+    .then((res) => {
+      return { pojo: res.data.data}
+    })
+  }
+}
+</script>
